b0d4b73cefb3916dcd8622423b161478f4536a1c
Annika Larson <annika@cole-larson.org>
2019-10-15 11:14:08 -0700

    Made intake actually stop

--- src/main/kotlin/frc/robot/Controls.kt
+++ src/main/kotlin/frc/robot/Controls.kt
@@ -33,8 +33,8 @@ object Controls : Updatable {
 
 //        button(kA).change(TurnInPlaceCommand(90.degree))
 
-        button(kB).changeOn(IntakeHatchCommand(false))
-        button(kY).changeOn(IntakeHatchCommand(true))
+        button(kB).change(IntakeHatchCommand(false))
+        button(kY).change(IntakeHatchCommand(true))
 //        button(kX).changeOn(CharacterizationCommand(DriveSubsystem))
 
         // Vision align
